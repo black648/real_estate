0680ad35a7195fc308da97cfb781bd3e1abbf773
HKL <bastlwalzer@gmail.com>
2023-02-05 15:22:04 +0900

    * Audit 설정 누락 추가 및 Point Exception 추가

--- src/main/java/com/real_estate/global/utils/CommonUtil.java
+++ src/main/java/com/real_estate/global/utils/CommonUtil.java
@@ -1,11 +1,16 @@
 package com.real_estate.global.utils;
 
 import org.locationtech.jts.geom.Point;
-import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKTReader;
 
 public class CommonUtil {
-    public static Point getLocation(Double longitude, Double latitude) throws ParseException {
-        return (Point) new WKTReader().read(String.format("POINT(%s %s)", longitude, latitude));
+    public static Point getLocation(Double longitude, Double latitude) {
+        try {
+            return (longitude == null || latitude == null ) ? null
+                    : (Point) new WKTReader().read(String.format("POINT(%s %s)", longitude, latitude));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
